072df9e7cb64e145a8b2de8635a6f1546414e75a
Syed Shahbaz Hussain <syed.hussain@lantmannen.com>
2020-11-15 01:39:34 +0100

    Added CinemaAppBackendUnitTest project

---
+++ CinemaApp/CinemaAppBackendUnitTest/CinemaAppBackendUnitTest.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+
+namespace CinemaAppBackendUnitTest
+{
+    public class CinemaBackendAppTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_ValidateCinemaDimensions()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void Test_ShowBookingStatus()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void Test_TicketBooking()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void Test_NumberOfPurchasedTickets()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void Test_PercentageOccupied()
+        {
+            Assert.Fail();
+        }
+        [Test]
+        public void Test_SumOfReservedTickets()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Test_PotentialTotalIncome()
+        {
+            Assert.Fail();
+        }
+    }
+}
